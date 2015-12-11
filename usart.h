1967ce29881e77dc0eca57b55a5b8c53aa4b4b2e
coolcapr-user <coolcapr-user@coolcapr-laptop>
2015-12-11 22:38:36 +0200

    Revised: rev1.3 - reworked all defines

--- usart.h
+++ usart.h
@@ -10,41 +10,49 @@
 
 #include "sys.h"
 
+// USART Flags
+#define USART_AutoBaudOverflow              (BAUDCONbits.ABDOVF)
+#define USART_ReceiveIdle                   (BAUDCONbits.RCIDL)
+#define USART_RxDataAvailable               (PIR1bits.RCIF)
+#define USART_TxBufferFull                  (!PIR1bits.TXIF)
+#define USART_FramingError                  (RCSTAbits.FERR)
+#define USART_OverunError                   (RCSTAbits.OERR)
+#define USART_TxBusy                        (!TXSTAbits.TRMT)
+
 //BAUDCON VALUES
-#define BAUDCON_DEFAULT                 0b00000000
-#define CLK_IDLE_STATE_HIGH             0b00010000
-#define BAUDRATE_GEN_16BIT              0b00001000
-#define AUTO_BAUDRATE_DETECT_ENABLE     0b00000001
+#define USART_BAUDCON_DEFAULT               0b00000000
+#define USART_CLK_IDLE_STATE_HIGH           0b00010000
+#define USART_BAUDRATE_GEN_16BIT            0b00001000
+#define USART_AUTO_BAUDRATE_DETECT_ENABLE   0b00000001
 
 //RCSTA VALUES
-#define RCSTA_DEFAULT                   0b00000000
-#define SERIAL_PORT_ENABLE              0b10000000
-#define RX_9BIT_MODE                    0b01000000
-#define SINGLE_RX_ENABLE                0b00100000
-#define CONT_RX_ENABLE                  0b00010000
-#define ADDRESS_DETECT_ENABLE           0b00001000
+#define USART_RCSTA_DEFAULT                 0b00000000
+#define USART_SERIAL_PORT_ENABLE            0b10000000
+#define USART_RX_9BIT_MODE                  0b01000000
+#define USART_SINGLE_RX_ENABLE              0b00100000
+#define USART_CONT_RX_ENABLE                0b00010000
+#define USART_ADDRESS_DETECT_ENABLE         0b00001000
 
 //TXSTA VALUES
-#define TXSTA_DEFAULT                   0b00000000
-#define INTERNAL_CLK_GENERATION         0b10000000
-#define TX_9BIT_MODE                    0b01000000
-#define TX_ENABLE                       0b00100000
-#define SYNCH_MODE_ENABLE               0b00010000
-#define HIGH_BAUDRATE                   0b00000100
+#define USART_TXSTA_DEFAULT                 0b00000000
+#define USART_INTERNAL_CLK_GENERATION       0b10000000
+#define USART_TX_9BIT_MODE                  0b01000000
+#define USART_TX_ENABLE                     0b00100000
+#define USART_SYNCH_MODE_ENABLE             0b00010000
+#define USART_HIGH_BAUDRATE                 0b00000100
 
-
-//Structure for USART TX and RX bits on PORTC RC6(TX) and RC7(RX) respectively
-typedef union
-{
-    struct
-    {
-        unsigned    NOT_USED        :6; //RC0 - RC5
-        unsigned    USART_TXD       :1; //RC6
-        unsigned    USART_RXD       :1; //RC7
-    };
-}USARTbits_t;
-volatile USARTbits_t USARTbits @ &PORTC;
-
+//USART_function defines
+#define USART_WakeUpModeEnable()            (BAUDCONbits.WUE = 1)
+#define USART_WakeUpModeDisable()           (BAUDCONbits.WUE = 0)
+#define USART_SerialPortEnable()            (RCSTAbits.SPEN = 1)
+#define USART_SerialPortDisable()           (RCSTAbits.SPEN = 0)
+#define USART_ContRxEnable()                (RCSTAbits.CREN = 1)
+#define USART_ContRxDisable()               (RCSTAbits.CREN = 0)
+#define USART_SingleRxEnable()              (RCSTAbits.SREN = 1)
+#define USART_SingleRxDisable()             (RCSTAbits.SREN = 0)
+#define USART_TxEnable()                    (TXSTAbits.TXEN = 1)
+#define USART_TxDisable()                   (TXSTAbits.TXEN = 0)
+#define USART_SendBreakCharacter()          (TXSTAbits.SENDB = 1)
 
 //USART related functions
 void USART_init(const uint8_t baudcon_val, const uint8_t rcsta_val, const uint8_t txsta_val, const uint16_t spbrg_val);
