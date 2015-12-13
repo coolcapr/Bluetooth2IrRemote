a374d9b480420634bafda34a9ece1b2582f5e7c3
coolcapr-user <coolcapr-user@coolcapr-laptop>
2015-12-13 21:05:19 +0200

    Added Timeout Function for USART receiver

--- usart.c
+++ usart.c
@@ -1,5 +1,8 @@
 #include "usart.h"
 
+#define TimeoutOccured              (PIR1bits.TMR1IF)
+#define RECEIVE_TIMEOUT             10000 //10 milliseconds
+
 void USART_Init(const uint8_t baudcon_val, const uint8_t rcsta_val, const uint8_t txsta_val, const uint16_t spbrg_val)
 {
     BAUDCON = baudcon_val;
@@ -8,6 +11,22 @@ void USART_Init(const uint8_t baudcon_val, const uint8_t rcsta_val, const uint8_
     
     RCSTA = rcsta_val;
     TXSTA = txsta_val;   
+}
+
+void SetTimeout(uint16_t Period)
+{
+    T1CONbits.TMR1ON = 0x01; //Switch On Timer1 
+    
+    Period = Period << 2; // multiply period by 4 since one instruction is 1/4 micro second @ 16MHz
+    CCPR1H = Period >> 8; //Mask high byte
+    CCPR1L = Period & 0x00FF; // Mask low byte
+    TMR1 = 0; //Reset timer 1
+    PIR1bits.TMR1IF = 0; //Clear timer1 interrupt flag
+}
+
+void RemoveTimeout()
+{
+    T1CONbits.TMR1ON = 0x00; //Switch Off Timer1
 }
 
 void USART_Putch(const uint8_t _char)
@@ -39,7 +58,15 @@ bool USART_Gets(uint8_t *buffer_ptr, uint8_t length)
     uint8_t index;
     for(index = 0; index < length; index++)
     {
-        while(!USART_RxDataAvailable){};
+        SetTimeout(RECEIVE_TIMEOUT);
+        while(!USART_RxDataAvailable)
+        {
+            if(TimeoutOccured)
+            {
+                return false;
+            }
+        }
+        RemoveTimeout();  
         
         if(USART_FramingError || USART_OverunError) //If overflow error,reset UART module
         {
@@ -68,7 +95,16 @@ bool USART_Read(uint8_t *buffer_ptr, const char delim)
 {
     do
     {
-        while(!USART_RxDataAvailable){};
+        SetTimeout(RECEIVE_TIMEOUT);
+        while(!USART_RxDataAvailable)
+        {
+            if(TimeoutOccured)
+            {
+                return false;
+            }
+        }   
+        RemoveTimeout(); 
+        
         
         if(USART_FramingError || USART_OverunError) //If overflow error,reset UART module
         {
